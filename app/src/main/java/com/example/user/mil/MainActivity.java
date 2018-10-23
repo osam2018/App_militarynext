f6d76e582c738ccf63bb04cac64ae1c0dfaa7d4d
youngje jo <siosio34@nate.com>
2018-10-23 22:50:47 +0900

    Add community class

--- app/src/main/java/com/example/user/mil/MainActivity.java
+++ app/src/main/java/com/example/user/mil/MainActivity.java
@@ -4,7 +4,8 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-import com.example.user.mil.view.store.StoreActivity;
+import com.example.user.mil.view.auth.SignupActivity;
+
 
 public class MainActivity extends AppCompatActivity {
 
@@ -13,7 +14,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Intent intent = new Intent(this, StoreActivity.class);
+        Intent intent = new Intent(this, SignupActivity.class);
         startActivity(intent);
 
     }
---
+++ app/src/main/java/com/example/user/mil/view/store/DeleteProductActivity.java
@@ -0,0 +1,15 @@
+package com.example.user.mil.view.store;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.user.mil.R;
+
+public class DeleteProductActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_delete_product);
+    }
+}
