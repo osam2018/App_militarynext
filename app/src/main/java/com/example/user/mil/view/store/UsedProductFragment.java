0bc6b92dc7c256c3057ce61f00346a3b82e4051f
youngje jo <siosio34@nate.com>
2018-10-23 14:24:29 +0900

    Add sample data

--- app/src/main/java/com/example/user/mil/view/store/UsedProductFragment.java
+++ app/src/main/java/com/example/user/mil/view/store/UsedProductFragment.java
@@ -4,6 +4,7 @@ import android.content.Context;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.GridLayoutManager;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
@@ -40,11 +41,25 @@ public class UsedProductFragment extends Fragment {
     private OnFragmentInteractionListener mListener;
 
     public void initRecyclerView() {
+
+        ArrayList arrayList = new ArrayList();
+        arrayList.add(new UsedProduct("제목임",15000,"운동기구","https://cdn-images-1.medium.com/max/800/1*M6HU7jlyQhmV8CjFcKdQhw.png",13,12,"zz","ss", "zz"));
+        arrayList.add(new UsedProduct("제목임",15000,"운동기구","https://cdn-images-1.medium.com/max/800/1*M6HU7jlyQhmV8CjFcKdQhw.png",13,12,"zz","ss", "zz"));
+        arrayList.add(new UsedProduct("제목임",15000,"운동기구","https://cdn-images-1.medium.com/max/800/1*M6HU7jlyQhmV8CjFcKdQhw.png",13,12,"zz","ss", "zz"));
+        arrayList.add(new UsedProduct("제목임",15000,"운동기구","https://cdn-images-1.medium.com/max/800/1*M6HU7jlyQhmV8CjFcKdQhw.png",13,12,"zz","ss", "zz"));
+        arrayList.add(new UsedProduct("제목임",15000,"운동기구","https://cdn-images-1.medium.com/max/800/1*M6HU7jlyQhmV8CjFcKdQhw.png",13,12,"zz","ss", "zz"));
+
+        UsedProductRecyclerViewAdapter adapter = new UsedProductRecyclerViewAdapter(getContext(), arrayList);
+        usedProductRecycler.setAdapter(adapter);
+
+        GridLayoutManager manager = new GridLayoutManager(getContext(), 2, GridLayoutManager.VERTICAL, false);
+        usedProductRecycler.setLayoutManager(manager);
+
 //        usedProductLayoutManager = new LinearLayoutManager(getContext(), LinearLayoutManager.VERTICAL, false);
 //        usedProductAdapter = new LinearLayoutManager(usedProducts);
 
-        usedProductRecycler.setAdapter(usedProductAdapter);
-        usedProductRecycler.setLayoutManager(usedProductLayoutManager);
+//        usedProductRecycler.setAdapter(usedProductAdapter);
+//        usedProductRecycler.setLayoutManager(usedProductLayoutManager);
     }
 
     public void loadUsedProductItems() {
@@ -74,6 +89,7 @@ public class UsedProductFragment extends Fragment {
         View view = inflater.inflate(R.layout.fragment_used_product, container, false);
         ButterKnife.bind(this, view);
 
+        initRecyclerView();
 
         return view;
     }
